738b9a3863b3b5e73b915349709b67754dc7bb0a
Ben Scheib <BenS@BenS-MBP15h.local>
2015-10-08 12:02:52 -0400

    updated jquery

--- app/public/jquery.js
+++ app/public/jquery.js
@@ -1,116 +1,95 @@
-
-( function( parent_doc ){
-
-// avoids parent constructor
-// for mixin approach
-function clone(obj) {
-  function F() { }
-  F.prototype = obj;
-  return new F();
-}
-
-var ElementArray = function() {
+(function( parent_doc ){
 	
-	// custom constructor for Array
-	this.push.apply(this, arguments);
-
-}
-
-ElementArray.prototype = clone(Array.prototype);
-
-var jQuery = function( selector, context ) {
-	
-	var obj = this;
-	
-	obj.selector = selector;
-	obj.context = context;
-	
-	if( false === this instanceof(jQuery) ) {
-		return new jQuery( selector, context );
-	}
-	
-	
+	// init function for new jquery object - hidden by closure
 	var init = function(selector, context) {
-	if( selector !== undefined ) {
-		if( typeof(selector) === 'string' ) 
-		{	
-			
-			var els = jQuery.find.apply( obj, [selector, context] );
-			return els;
-		} else if (typeof(selector) === 'object') {
-			return new ElementArray(selector)
+		var obj = this;
+		
+		obj.selector = selector;
+		obj.context = context;
+		
+		if( false === this instanceof(jQuery) ) {
+			return new jQuery( selector, context );
 		}
-	} else {
-		return new ElementArray();
+		
+		if( typeof(selector) === 'string' ) {
+			return els = obj.find( selector, context );
+		} else  {
+			return new ElementArray( selector, context);
+		}
 	}
-}
-	
-	return new init( selector, context );
-}
 
-
-jQuery.fn = jQuery.prototype = {
-	//constructor: jQuery,
+	var jQuery = function( selector, context ) {
+		return init.call(this, selector, context );
+	};
 	
-	find: function(selector ) {
+	jQuery.fn = jQuery.prototype = {
+		constructor: jQuery,
 		
-		var arr = new ElementArray();
-		
-		var find_by_parent = function( selector, parent, container_arr ) {
+		find: function(selector ) {
 			
-			if( container_arr === undefined )
-				container_arr = [];
-						
-			var els = parent.querySelectorAll(selector);
-			var i = 0;
-			for( var i = 0; i < els.length; i++) 
-				container_arr.push( els[i] );
+			var arr = new ElementArray();
+			
+			var find_by_parent = function( selector, parent, container_arr ) {
 				
-			return container_arr;
-			
-		}
-		
-		// do the find for all the 'jquery' elements
-		if( true === (this instanceof(ElementArray)) ) {
-			
-			for( var j = 0; j < this.length; j++) 
-				find_by_parent( selector, this[j], arr);
+				if( container_arr === undefined )
+					container_arr = [];
+							
+				var els = parent.querySelectorAll(selector);
+				var i = 0;
+				for( var i = 0; i < els.length; i++) 
+					container_arr.push( els[i] );
+					
+				return container_arr;
 				
+			}
 			
-		} else {
+			// do the find for all the 'jquery' elements
+			if( true === (this instanceof(ElementArray)) ) {
+				
+				for( var j = 0; j < this.length; j++) 
+					find_by_parent( selector, this[j], arr);
+					
+				
+			} else {
+				
+			    var parent = document;
+			    if( this.context ) {
+				    parent = find_by_parent( this.context, document);
+				   
+				    if( parent.length ) 
+					    parent = parent[0];
+					    
+			    }
+				
+				find_by_parent( selector, parent, arr );
+				
+			}
 			
-			// dont think this will work with chaining though :(
-		    // need to get the parent
-		    var parent = document;
-		    if( this.context ) {
-			    parent = find_by_parent( this.context, document);
-			   
-			    if( parent.length ) 
-				    parent = parent[0];
-				    
-		    }
-			
-			find_by_parent( selector, parent, arr );
-			
+			return arr;
 		}
-		
-		return arr;
 	}
-}
-
-
-function extend(destination, source) {
-  for (var k in source) {
-    if (source.hasOwnProperty(k)) {
-      destination[k] = source[k];
-    }
-  }
-  return destination; 
-}
-
-extend(ElementArray.prototype, jQuery.prototype);
-extend(jQuery, jQuery.fn); // we want these to be static methods
-
-parent_doc.$ = parent_doc.jQuery = jQuery;
+	
+	// Array like structure to store elements
+	var ElementArray = function( selector, context ) {
+		if ( selector && typeof(selector) === 'object') {
+			this.push(selector);
+		}
+	}
+	ElementArray.prototype = Object.create(jQuery.prototype);
+	
+	ElementArray.prototype.constructor = ElementArray;
+	ElementArray.prototype.length = 0;
+	ElementArray.prototype.splice = Array.prototype.splice;
+	ElementArray.prototype.push = Array.prototype.push;
+	
+	// insert jQuery.fn static methods to jQuery
+	for (var k in jQuery.fn) {
+		if (jQuery.fn.hasOwnProperty(k)) {
+		  jQuery[k] = jQuery.fn[k];
+		}
+	}
+	
+	// set the global variables
+	parent_doc.$ = parent_doc.jQuery = jQuery;
 
 })( window )
